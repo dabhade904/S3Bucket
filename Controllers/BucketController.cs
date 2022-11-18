927cf873b4eeb81b09092669591309e3713a2a01
dabhade904 <dabhade904@gmail.com>
2022-11-18 11:03:19 +0530

    Added : Project to add file to s3 bucket

---
+++ Controllers/BucketController.cs
@@ -0,0 +1,94 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using S3BucketDemo.models;
+
+namespace S3BucketDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BucketController : ControllerBase
+    {
+        private readonly IAmazonS3 _s3Client;
+        public BucketController(IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateBucketAsync(string bucketName)
+        {
+            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
+            if (bucketExists) return BadRequest($"Bucket {bucketName} already exists.");
+            await _s3Client.PutBucketAsync(bucketName);
+            return Ok($"Bucket {bucketName} created.");
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllBucketAsync()
+        {
+            var data = await _s3Client.ListBucketsAsync();
+            var buckets = data.Buckets.Select(b => { return b.BucketName; });
+         le    return Ok(buckets);
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFileAsync(IFormFile file, string bucketName, string? prefix)
+        {
+            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var request = new PutObjectRequest()
+            {
+                BucketName = bucketName,
+                Key = string.IsNullOrEmpty(prefix) ? file.FileName : $"{prefix?.TrimEnd('/')}/{file.FileName}",
+                InputStream = file.OpenReadStream()
+            };
+            request.Metadata.Add("Content-Type", file.ContentType);
+            await _s3Client.PutObjectAsync(request);
+            return Ok($"File {prefix}/{file.FileName} uploaded to S3 successfully!");
+        }
+
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAllFilesAsync(string bucketName, string? prefix)
+        {
+            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var request = new ListObjectsV2Request()
+            {
+                BucketName = bucketName,
+                Prefix = prefix
+            };
+            var result = await _s3Client.ListObjectsV2Async(request);
+            var s3Objects = result.S3Objects.Select(s =>
+            {
+                var urlRequest = new GetPreSignedUrlRequest()
+                {
+                    BucketName = bucketName,
+                    Key = s.Key,
+                    Expires = DateTime.UtcNow.AddMinutes(1)
+                };
+                return new S3ObjectDto()
+                {
+                    Name = s.Key.ToString(),
+                    PresignedUrl = _s3Client.GetPreSignedURL(urlRequest),
+                };
+            });
+            return Ok(s3Objects);
+        }
+
+        [HttpPost("copy")]
+        public async Task<bool> CopyObjectInBucketAsync(string soursebucketName, string destinationbucketName, string objectName)
+        {
+            var request = new CopyObjectRequest
+            {
+                SourceBucket = soursebucketName,
+                SourceKey = objectName,
+                DestinationBucket = destinationbucketName,
+                DestinationKey = $"{objectName}",
+            };
+            var response = await _s3Client.CopyObjectAsync(request);
+            return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
+    }
+}
---
+++ models/S3ObjectDto.cs
@@ -0,0 +1,8 @@
+﻿namespace S3BucketDemo.models
+{
+    public class S3ObjectDto
+    {
+        public string? Name { get; set; }
+        public string? PresignedUrl { get; set; }
+    }
+}
